75e6ad423728f19ba60917db36f648194a4a5e49
wypark <wyp9679@gmail.com>
2025-11-03 15:10:54 +0900

    reconstructed: expose the domain type barrel

---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+export type * from './api';
+export type * from './auth';
+export type * from './category';
+export type * from './chess';
+export type * from './comment';
+export type * from './dashboard';
+export type * from './post';
+export type * from './profile';
